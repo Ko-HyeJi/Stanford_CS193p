007d354c9055ff67c35e86abad6df4a1cd6bdd7a
고혜지 <kohyeji@gohyejiui-MacBookPro.local>
2023-06-10 18:47:15 +0900

    Lecture 2

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 고혜지 on 2023/06/06.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
